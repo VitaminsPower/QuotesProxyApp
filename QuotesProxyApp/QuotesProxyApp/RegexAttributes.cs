833e7346bf49c603e4f46489dcc8731f2bb553fe
AndrewArch <a4.arhipenko@itransition.com>
2025-04-21 15:58:23 +0200

    removes unused code

--- QuotesProxyApp/QuotesProxyApp/RegexAttributes.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.RegularExpressions;
-using JetBrains.Annotations;
-
-[UsedImplicitly]
-internal partial class StartUp
-{
-    [GeneratedRegex(@"\b([A-Za-z]{6})\b")]
-    private static partial Regex SixLetterRegex();
-}
--- QuotesProxyApp/QuotesProxyApp/RegexList.cs
+++
@@ -1,7 +0,0 @@
-using System.Text.RegularExpressions;
-
-public static partial class RegexList
-{
-    [GeneratedRegex(@"\b([A-Za-z]{6})\b")]
-    private static partial Regex SixLettersRegex();
-}
